a2a9a33b36856f21bc696ca6bcd1e52ddbe74213
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-09-24 12:24:21 -0400

    Update area.js

--- area.js
+++ area.js
@@ -4,14 +4,13 @@ const client = new Dicord.Client();
 
 client.on(`ready`, () => {
   console.log(`I am ready!`);
-
+client.user.setActivity('with a toy ;)');
 });
 
-
-client.on("message", message => {
-if (message.content === `${prefix}ping`) {
-message.reply(`Hey there darling.~`);
-}
-});
+client.on('message', async (message) => {
+  
+    if (message.content === `${prefix}hello`) {
+    message.reply(`Bark!`);
+    }
 
 client.login(process.env.BOT_TOKEN);
