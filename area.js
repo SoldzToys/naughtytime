dc514994e705804e0dbe1c3c441b5d2421fc480a
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-09-24 12:26:33 -0400

    Update area.js

--- area.js
+++ area.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 const client = new Dicord.Client();
+const config = require('./botconfig.json');
+const { prefix, token } = require('./botconfig.json');
 
 
 client.on(`ready`, () => {
   console.log(`I am ready!`);
 client.user.setActivity('with a toy ;)');
+  
 });
 
 client.on('message', async (message) => {
   
     if (message.content === `${prefix}hello`) {
     message.reply(`Bark!`);
+      
     }
-
+});     
 client.login(process.env.BOT_TOKEN);
