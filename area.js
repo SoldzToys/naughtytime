ec647e415d39ef9c2c7ed7caf9473617b08345d3
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-09-24 12:29:50 -0400

    Update area.js

--- area.js
+++ area.js
@@ -10,11 +10,12 @@ client.user.setActivity('with a toy ;)');
   
 });
 
-client.on('message', async (message) => {
+client.on('message', message => {
   
     if (message.content === `${prefix}hello`) {
-    message.reply(`Bark!`);
+    message.reply(`Hey there darling.~`);
       
     }
+  
 });     
 client.login(process.env.BOT_TOKEN);
