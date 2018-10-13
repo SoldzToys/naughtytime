d9c3dd715aba7e3a8d841c20f983b47759746c54
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-13 18:03:56 -0400

    Update area.js

--- area.js
+++ area.js
@@ -11,7 +11,7 @@ const { prefix, token } = require('./botconfig.json');
 });
 
   
-client.on('message', async (client, message, args, level) => {
+client.on('message', async (client, message) => {
  const superagent = require("snekfetch");
  const Discord = require('discord.js');
 
