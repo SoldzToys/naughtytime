4f6de40a6de17ab0f799945a2dc9eec76d4e637b
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-13 17:13:00 -0400

    Update area.js

--- area.js
+++ area.js
@@ -4,18 +4,9 @@ const config = require('./botconfig.json');
 const { prefix, token } = require('./botconfig.json');
 
 
-client.on(`ready`, () => {
-  console.log(`I am ready!`);
-client.user.setActivity('with a toy ;)');
-  
+  client.on("ready", async () => {
+  console.log(`${client.user.username} is online!`);
+  client.user.setActivity("h!kisshentai");
 });
 
-client.on('message', message => {
-  
-    if (message.content === `${prefix}hello`) {
-    message.reply(`Hey there darling.~`);
-      
-    }
-  
-});     
 client.login(process.env.BOT_TOKEN);
