6c9c54c795a48070ff0f848dfd4617755a8dd568
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-16 16:26:04 -0400

    Update area.js

--- area.js
+++ area.js
@@ -835,7 +835,7 @@ message.channel.send(helpEmbed);
     .addField("Guilds", client.guilds.size, true)
     .addField("Users", client.users.size, true)
     .addField("Invite Me!~", "https://bit.ly/2AbmU0S", true)
-    .setFooter("Created By @Dawn.Bots.INC", "https://i.imgur.com/WYIX5AN.png") 
+    .setFooter("Created By @Dawn.Bots.INC", "https://i.imgur.com/kQodEGp.png") 
     .setTimestamp();
     return message.channel.send(botembed);
   }
