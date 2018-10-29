cb3d8f376f481247ecfbfc1fd49b46cf41a22415
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-29 18:11:56 -0400

    Update area.js

--- area.js
+++ area.js
@@ -304,12 +304,12 @@ client.on('message', async (message) => {
   let helpEmbed = new Discord.RichEmbed()
 .setTitle("😩 NSFW/SFW Command Menu 👌")
 .setDescription(`These are the comamnds for this bot, ones with the label SFW mean they can be used outside of NSFW while NSFW can only be used in NSFW channels. Also if you have never used bots like **Senpai**, **Miki** or **Naughty Time~**, when using most of the fun commands you must mention a user to use the command. Example: https://imgur.com/a/FeAsJzO`)
-.setColor("#b70000") 
+.setColor("#b70000")  
+.setImage(helpImage)
 .addField("❔Information Commands", "botinfo, serverinfo, useravatar, whois.")
 .addField("📘 Fun SFW Commands", "avatar, baka, cuddle, donate, feed, foxgirl, hug, kiss, lizard, meow, neko, pat, poke, slap, smug, tickle, waifu, wallpaper.")
 .addField("🔞 NSFW Commands", "anal, bjgif, blowjob, cum, eroticholo, erotease, erokemo1, erokemo2, eroyuri, feet, footjob, femdom, futa(futanari), hentai, hgif, hgirl, holoneko, kemo, lewdkemo, lewdnekogif, lewdmoji nsfwavatar, ngif, pussy, pussylick, randomhentai, smallboobs, sologirl, sologif, spank, trap, tits, yuri.")
-//.setImage("https://i.imgur.com/FrxGqHl.png");
-  .setImage(helpImage)
+//.setImage("https://i.imgur.com/FrxGqHl.png")
    .setFooter("f", `${client.user.displayAvatarURL}`)
     .setTimestamp();
 message.channel.send(helpEmbed);
